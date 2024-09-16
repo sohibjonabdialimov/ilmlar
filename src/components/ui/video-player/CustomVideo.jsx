1eee360f46537c964ae1d641e898ea2d43f9c27d
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-09-16 13:17:54 +0500

    done

--- src/components/ui/video-player/CustomVideo.jsx
+++ src/components/ui/video-player/CustomVideo.jsx
@@ -1,25 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import CustomVideoPlayer from './CustomVideoPlayer'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import CustomVideoPlayer from "./CustomVideoPlayer";
+import axios from "axios";
 
 function CustomVideo({ videosrc }) {
-  const [urls, seturls] = useState([]);
-  if(!videosrc){
-    return;
-  }
+  const [urls, setUrls] = useState([]);
+
   useEffect(() => {
-    axios.get(`${videosrc}`).then((res) => {
-      console.log(res);
-      seturls(res.data);
+    if (!videosrc) {
+      return;
+    }
+    const fetchVideo = async () => {
+      try {
+        const response = await axios.get(`${videosrc}`);
 
-    })
+        setUrls(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-  }, [videosrc])
-  return (
-    <div>
-    { urls.length > 0 && <CustomVideoPlayer urls={urls} /> }
-    </div>
-  )
+    fetchVideo();
+  }, [videosrc]);
+
+  return <div>{urls.length > 0 && <CustomVideoPlayer urls={urls} />}</div>;
 }
 
-export default CustomVideo
+export default CustomVideo;
