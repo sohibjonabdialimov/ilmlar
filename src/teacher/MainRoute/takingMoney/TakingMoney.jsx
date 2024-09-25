3c958c1623a5137a9cd7ddd59eb9ec8340b8cbe2
jaxmadov <jaxmadov@transoxiania.com>
2024-09-25 18:43:11 +0500

    same changes

--- src/teacher/MainRoute/takingMoney/TakingMoney.jsx
+++ src/teacher/MainRoute/takingMoney/TakingMoney.jsx
@@ -1,39 +1,87 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./style.module.css";
 import CurrencyFormat from "react-currency-format";
+import * as Yup from "yup";
+import { ToastContainer, toast } from "react-toastify";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import axios from "axios";
+
 function TakingMoney() {
   const navigate = useNavigate();
   const inputCardRef = useRef();
+  const pniflref = useRef();
   const [card, setCard] = useState();
+
+  const validationSchema = Yup.object({
+    cardNumber: Yup.string()
+      .transform((value) => value.replace(/\s+/g, "")) // Probellarni olib tashlaymiz
+      .min(16, "Karta raqami kamida 16 ta raqamdan iborat bo'lishi kerak")
+      .max(16, "Karta raqami 16 ta raqamdan oshmasligi kerak")
+      .required("Field is required"),
+      pinfl: Yup.string()
+      .min(14, "pinfl kamida 14 ta raqamdan iborat bo'lishi kerak")
+      .max(14, "pinfl 14 ta raqamdan oshmasligi kerak")
+      .required("Field is required"),
+    amount: Yup.number()
+      .min(1000, "Minimal miqdor 1000 UZS bo'lishi kerak")
+      .required("Field is required"),
+  });
+
+  const initialValues = {
+    cardNumber: "",
+    pinfl: "",
+    amount: 0,
+  };
+
   const onBack = () => {
     navigate(-1);
   };
 
-  const onHandleClick = (e) => {
-    e.preventDefault();
-    navigate("/hisoblar/pulyichish/ok");
-  };
-  const handleCardNumberChange = () => {
-    const cardValue = inputCardRef.current.value
+  const handleCardNumberChange = (e, setFieldValue) => {
+    const cardValue = e.target.value
       .replace(/\D/g, "")
       .match(/(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})/);
-      console.log(cardValue);
-      
-    inputCardRef.current.value = !cardValue[2]
+
+    const formattedCardNumber = !cardValue[2]
       ? cardValue[1]
-      : `${cardValue[1]} ${cardValue[2]}${`${
-          cardValue[3] ? ` ${cardValue[3]}` : ""
-        }`}${`${cardValue[4] ? ` ${cardValue[4]}` : ""}`}`;
-    const numbers = inputCardRef.current.value.replace(/(\D)/g, "");
-    setCard(numbers);
+      : `${cardValue[1]} ${cardValue[2]}${`${cardValue[3] ? ` ${cardValue[3]}` : ""}`}${`${cardValue[4] ? ` ${cardValue[4]}` : ""}`}`;
+
+    setFieldValue("cardNumber", formattedCardNumber);
+    setCard(formattedCardNumber);
   };
 
-  useEffect(() => {
-    handleCardNumberChange();
-  }, [card]);
+  const handleSubmit = async (values) => {
+    const formattedValues = {
+      ...values,
+      cardNumber: values.cardNumber.replace(/\s+/g, ""), // bo'shliqlarni olib tashlaymiz
+    };
+  
+    console.log(formattedValues);
+  
+    await axios
+      .post(
+        `${import.meta.env.VITE_API_KEY}/cridettotecher/`,
+        formattedValues,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      )
+      .then((res) => {
+        if (res.status === 201) {
+          toast.success("payment is success");
+          navigate("/hisoblar/pulyichish/ok");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(err.response.data.message);
+      });
+  };
 
- 
   return (
     <div className="app-content">
       <div className="global_wrap">
@@ -41,41 +89,82 @@ function TakingMoney() {
           <button onClick={onBack} className={styles.back}>
             <ion-icon name="chevron-back-outline"></ion-icon>
           </button>
-          <form
-            onSubmit={(e) => onHandleClick(e)}
-            className={styles.hisob_form}
-          >
-            <div className={styles.input_wrap}>
-              <label htmlFor="amount">Pul miqdori</label>
-              <div className={styles.input_div}>
-              <CurrencyFormat thousandSeparator={true} placeholder={"Summa, UZS"} suffix={' UZS'} />
-              </div>
-            </div>
-            <div className={styles.input_wrap}>
-              <div className={styles.label_wrap}>
-                <label>Karta raqami</label>
-                <label>Humo / UzCard </label>
-              </div>
 
-              <div className={styles.input_div}>
-                <input
-                  type="text"
-                  placeholder="xxxx   xxxx   xxxx   xxxx"
-                  ref={inputCardRef}
-                  onChange={handleCardNumberChange}
-                />
-              </div>
-            </div>
-            <div className={styles.input_wrap}>
-              <label htmlFor="amount">
-                Parol ( sahifangizga kirish paroli )
-              </label>
-              <div className={styles.input_div}>
-                <input type="text" />
-              </div>
-            </div>
-            <button type="submit">Pulni yechib olish</button>
-          </form>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ values, handleChange, setFieldValue }) => (
+              <Form className={styles.hisob_form}>
+                <div className={styles.input_wrap}>
+                  <label htmlFor="amount">Pul miqdori</label>
+                  <div className={styles.input_div}>
+                    <CurrencyFormat
+                      thousandSeparator={true}
+                      placeholder="Summa, UZS"
+                      suffix=" UZS"
+                      value={values.amount}
+                      onValueChange={(val) => {
+                        setFieldValue("amount", val.floatValue || 0); // Convert to number
+                      }}
+                    />
+                    <ErrorMessage
+                      name="amount"
+                      component="div"
+                      className={styles.error}
+                    />
+                  </div>
+                </div>
+
+                <div className={styles.input_wrap}>
+                  <div className={styles.label_wrap}>
+                    <label>Karta raqami</label>
+                    <label>Humo / UzCard</label>
+                  </div>
+
+                  <div className={styles.input_div}>
+                    <input
+                      name="cardNumber"
+                      type="text"
+                      placeholder="xxxx   xxxx   xxxx   xxxx"
+
+                      value={values.cardNumber}
+                      onChange={(e) => handleCardNumberChange(e, setFieldValue)}
+                      ref={inputCardRef}
+                    />
+                    <ErrorMessage
+                      name="cardNumber"
+                      component="div"
+                      className={styles.error}
+                    />
+                  </div>
+                </div>
+
+                <div className={styles.input_wrap}>
+                  <div className={styles.label_wrap}>
+                    <label>PNIFL</label>
+                  </div>
+
+                  <div className={styles.input_div}>
+                    <Field
+                      name="pinfl"
+                      placeholder="xxxxxxxxxxxxxx"
+                      type="text"
+                      ref={pniflref}
+                    />
+                    <ErrorMessage
+                      name="pinfl"
+                      component="div"
+                      className={styles.error}
+                    />
+                  </div>
+                </div>
+
+                <button type="submit">Pulni yechib olish</button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
